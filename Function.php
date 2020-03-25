fbd390b9ca42ee817cdba88c1681c88a4c33620d
MrApple <32264784+MrBigAppleTree@users.noreply.github.com>
2020-03-25 22:53:28 +0100

    Add basic user administration
    Now you can add new accounts!

--- Function.php
+++ Function.php
@@ -1,6 +1,7 @@
 <?php 
 require("Connect.php");
 
+//LoginScreen.php
 function validate($str) 
 {
   return trim(htmlspecialchars($str));
@@ -10,38 +11,30 @@ function validateUser($Username, $Password)
 {
   $conn = connectDB();
   
-  if (strlen($BridePass) > 5)
+  try
   {
-    try
+    $sql = "SELECT Wachtwoord FROM gebruiker WHERE `UniekeLoginNaam` = :Username";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue("Username", $Username, PDO::PARAM_STR);
+    $stmt->execute();
+    foreach ($stmt->fetchAll() as $row) 
     {
-      $sql = "SELECT Wachtwoord FROM gebruiker WHERE `UniekeLoginNaam` = :Username";
-      $stmt = $conn->prepare($sql);
-      $stmt->bindValue("Username", $Username, PDO::PARAM_STR);
-      $stmt->execute();
-      foreach ($stmt->fetchAll() as $row) 
-      {
-        $result = $row["PassWD"];
-      }
-    }
-    catch (exception $ex)
-    {
-      return false;
-    }
-    if (password_verify($Password, $result))
-    {
-      return true;
-    }
-    else 
-    {
-      return false;
+      $result = $row["Wachtwoord"];
     }
   }
-  else
+  catch (exception $ex)
+  {
+    return false;
+  }
+  if (password_verify($Password, $result))
+  {
+    return true;
+  }
+  else 
   {
     return false;
   }
 }
-
 
 if ((!empty($_POST["Login"])) && ($_POST["Login"] == "Inloggen"))
 {
@@ -50,7 +43,7 @@ if ((!empty($_POST["Login"])) && ($_POST["Login"] == "Inloggen"))
   $Password = filter_var($_POST["Password"], FILTER_SANITIZE_STRING);
   $Password = validate($Password);
 
-  if (!empty($_POST["Username"]) && (!empty($_POST["Password"])) && validateBride($BrideLoginCode, $BridePass))
+  if (!empty($_POST["Username"]) && (!empty($_POST["Password"])) && validateUser($Username, $Password))
   {
     if (session_status() == PHP_SESSION_NONE) 
     {
@@ -64,4 +57,70 @@ if ((!empty($_POST["Login"])) && ($_POST["Login"] == "Inloggen"))
     echo"De ingevoerde gegevens zijn incorrect.";
   }
 }
+//LoginScreen.php ^^^^
+
+
+//UserAdministration.php
+if ((!empty($_POST["CreateAcc"])) && ($_POST["CreateAcc"] == "Aanmaken"))
+{
+  validateNewAccount();
+}
+
+function validateNewAccount()
+{
+  $conn = connectDB();
+
+  $Password = filter_var($_POST["Password"], FILTER_SANITIZE_STRING);
+  $Password = validate($Password);
+  $Password2 = filter_var($_POST["Password2"], FILTER_SANITIZE_STRING);
+  $Password2 = validate($Password2);
+  $Hashed = password_hash($Password, PASSWORD_DEFAULT);
+  $Rights = $_POST["Rights"];
+  $Username = $_POST["Username"];
+
+  try
+  {
+    $sql = "SELECT UniekeLoginNaam FROM gebruiker WHERE UniekeLoginNaam = :Username";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue("Username", $Username, PDO::PARAM_STR);
+    $stmt->execute();
+    foreach ($stmt->fetchAll() as $row) 
+    {
+      $Result = $row["UniekeLoginNaam"];
+    }
+    if ((!($Result == $Username)) && ($password == $password2) && (strlen($Username) > 2))
+    {
+      $sql = "INSERT INTO `gebruiker` (`GebruikerID`, `UniekeLoginNaam`, `Wachtwoord`, `Rechten`) VALUES (NULL, :Username, :Password, :Rights);";
+
+      $stmt = $conn->prepare($sql);
+      $stmt->bindValue("Password", $Hashed, PDO::PARAM_STR);
+      $stmt->bindValue("Username", $Username, PDO::PARAM_STR);
+      $stmt->bindValue("Rights", $Rights, PDO::PARAM_STR);
+      if($stmt->execute()) 
+      {
+        echo "Het account is succesvol aangemaakt!";
+      }
+    }
+    else
+    {
+      if ($Result == $Username)
+      {
+        echo "Deze gebruikersnaam is al in gebruik.";
+      }
+      elseif (strlen($Username) < 3)
+      {
+        echo "Je gebruikersnaam is te kort.";
+      }
+      if (!($Password == $Password2))
+      {
+        echo "De wachtwoorden komen niet overeen.";
+      }
+    }
+  }
+  catch(exception $ex)
+  {
+    echo "Er was een fout met de connectie met het database.";
+  }
+}
+//UserAdministration.php ^^^^
 ?>
---
+++ UserAdministration.php
@@ -0,0 +1,29 @@
+<?php 
+  require("Function.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 4 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<style>
+
+</style>
+</head>
+<body>
+<form action="UserAdministration.php" method="post">
+  Gebruikersnaam:<br><input type="str" name="Username"><br>
+  Wachtwoord:<br><input type="str" name="Password"><br>
+  Wachtwoord hertypen:<br><input type="str" name="Password2"><br><br>
+  <select name="Rights">
+    <option value="0">Lezer</option>
+    <option value="1">Administrator</option>
+  </select><br><br>
+  <input type="submit" name="CreateAcc" value="Aanmaken">
+</form><br>
+</div>
+</body>
+</html>
